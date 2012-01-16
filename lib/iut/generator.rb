2d8065320bf1027b9fbd6d98d127bfc86de17800
Katsuyoshi Ito <kito@itosoft.com>
2012-01-17 00:08:49 +0900

    fixed a path

--- lib/iut/generator.rb
+++ lib/iut/generator.rb
@@ -6,13 +6,13 @@ module Iut
 
   module Generator
   
-    TEMPLATE_PATH = File.expand_path(File.join(File.basename(__FILE__), "../../template"))
+    TEMPLATE_PATH = File.join(File.expand_path(File.dirname(__FILE__)), "../../template")
     
     module Project
     
       def self.generate
         src = File.join(TEMPLATE_PATH, "project", "iUnitTest")
-        dst = File.expand_path(File.join("./", ARGV[0]))
+        dst = File.join(File.expand_path("./"), ARGV[0])
         if File.exists?(dst)
           puts "#{ARGV[0]}: File exists"
           exit
