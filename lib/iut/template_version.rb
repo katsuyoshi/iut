f24b775828dac64a0bdc70f45304722b5e1f4b8a
Katsuyoshi Ito <kito@itosoft.com>
2012-02-19 22:29:18 +0900

    update template

--- lib/iut/template_version.rb
+++ lib/iut/template_version.rb
@@ -1,4 +1,4 @@
 # -*- encoding:UTF-8 -*-
 module Iut
-  TEMPLATE_VERSION = "de6ccdc"
+  TEMPLATE_VERSION = "f7c799a"
 end
--- lib/iut/version.rb
+++ lib/iut/version.rb
@@ -1,4 +1,4 @@
 # -*- encoding:UTF-8 -*-
 module Iut
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
