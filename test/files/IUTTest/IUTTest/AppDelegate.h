62f3fba13ab306b43975f797e60105b0965f80aa
Katsuyoshi Ito <kito@itosoft.com>
2012-02-29 08:38:10 +0900

    added TestProject

---
+++ test/files/IUTTest/IUTTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IUTTest
+//
+//  Created by Ito Katsuyoshi on 12/02/29.
+//  Copyright (c) 2012年 ITO SOFT DESIGN Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
